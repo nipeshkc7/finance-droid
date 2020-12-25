e23f82bbf9c9a52c1473c85921088a261aef5ac2
Arpan Kc <nipeshkc7@gmail.com>
2020-12-25 20:34:59 +1100

    added index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('service is alive');
+})
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, ()=>{
+  console.log(`App listening on port: ${port}`);
+})
