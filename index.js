c803316b8a3c3e9cbd4df3983e888c2cfe221a2f
Arpan Kc <nipeshkc7@gmail.com>
2020-12-25 20:54:16 +1100

    adding get-stock-price route

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const quote = require('@stonksjs/quote');
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('service is alive');
-})
+});
+
+app.get('/get-stock-price', async(req, res) => {
+  const symbol = req.query.symbol;
+
+  if(!symbol){
+    return res.status(400).end('No symbol specified');
+  }
+
+  try{
+    const data = await quote(symbol);
+    res.json(data);
+  } catch(err){
+    res.end(`Error finding the stock. Please try a different symbol ! ERR: ${err.message}`);
+  }
+});
 
 const port = process.env.PORT || 8080;
 
